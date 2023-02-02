52e3ff1b9fa711bd5a99ba640535b2e762795efd
sonvannguyen <sonnv686.com>
2023-02-03 00:48:51 +0700

    create api delete and update movie

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const MovieModel = require('../models/Movie')
+const MovieGroupModel = require('../models/MovieGroup')
 
 // xử lí khi người dùng tìm kiếm tên phim mà không gõ dấu . Tận dụng slug có sẵn trong db để tìm kiếm.
 
@@ -31,7 +32,7 @@ const movieController = {
         //             await newMovie.save()
         //             console.log('save ', index)
         //         })
-        //         console.log('SUCCESSSSSSSSSSSSSSSSSSSSSS')
+        //         console.log('SUCCESS')
         //     });
             
         // } catch (error) {
@@ -41,14 +42,48 @@ const movieController = {
     
     createMovie: async (req, res, next) => {
         try {
-            const newMovie = new MovieModel(req.body)
-            await newMovie.save()
+            // handle array category when send many values in body
+            const {category:categoryRaw} = req.body
+            category = categoryRaw?.split(',')
+
+            const newMovie = new MovieModel({...req.body, category })
+            await newMovie.save()   
+
             return res.json({newMovie})
         } catch (error) {
             next(error)
         }
     },
-    
+    updateMovie: async (req, res, next) => {
+        try {
+            const {movieId} = req.params
+            const newMovie = await MovieModel.findByIdAndUpdate(movieId, req.body, {new:true})
+            await newMovie.save()
+
+            return res.json({newMovie})
+        } catch (error) {
+            next(error)
+        }
+    },
+    deleteMovie: async (req, res, next) => {
+        try {
+            const {movieId} = req.params
+            await MovieModel.findByIdAndDelete(movieId)
+
+            const movieGroup = await MovieGroupModel.findOne({
+                movieList: { $in: [movieId]}
+            })
+
+            if(movieGroup){
+                movieGroup.movieList = movieGroup.movieList.filter(movie => movie != movieId)
+                await movieGroup.save()
+            }
+
+            return res.json({movieGroup})
+        } catch (error) {
+            next(error)
+        }
+    },
     getAllMovie: async(req, res, next) => {
         try {
             const page = parseInt(req.query.page) || 1
--- server/routes/movieRoute.js
+++ server/routes/movieRoute.js
@@ -2,9 +2,11 @@ const router = require('express').Router()
 const movieController = require('../controllers/movieController')
 
 router.get('/', movieController.getAllMovie)
-router.post('/create', movieController.createMovie)
 router.get('/search', movieController.searchMovieByName)
 router.get('/filter', movieController.filterMoviesByMultiField)
 
+router.post('/create', movieController.createMovie)
+router.put('/update/:movieId', movieController.updateMovie)
+router.delete('/delete/:movieId', movieController.deleteMovie)
 
 module.exports = router
